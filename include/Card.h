301b977335395b85af8dd197fd355d6ad4da7efb
Justin <jbush023@uottawa.ca>
2024-11-18 16:15:47 -0500

    Added derived classes for all beans with implementation of coins/card, getName, print methods

--- include/Card.h
+++ include/Card.h
@@ -6,12 +6,17 @@
 
 class Card {
 public:
+    Card() = default; 
     virtual ~Card() = default;
+
     virtual int getCardsPerCoin(int coins) const = 0;
     virtual std::string getName() const = 0;
     virtual void print(std::ostream& out) const = 0;
 
-    friend std::ostream& operator<<(std::ostream& out, const Card& card);
+    friend std::ostream& operator<<(std::ostream& out, const Card& card) {
+        card.print(out);
+        return out;
+    }
 };
 
 #endif
