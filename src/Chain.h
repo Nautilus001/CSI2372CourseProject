144cd6463dabd10aabb250da0c0875b0ed53c623
LD-UO <ldusa012@uottawa.ca>
2024-12-03 22:04:31 -0500

    moving the getName() function

--- src/Chain.h
+++ src/Chain.h
@@ -6,74 +6,94 @@
 #include "Card.h"
 #include "ChainBase.h"
 
-class IllegalTypeException: public std::exception{
-    virtual const char* what() const throw(){
+class IllegalTypeException : public std::exception
+{
+    virtual const char *what() const throw()
+    {
         return "IllegalTypeException";
     }
 };
 
 template <typename T>
-class Chain : public ChainBase {   
+class Chain : public ChainBase
+{
 private:
     std::vector<Card *> cards;
-    const std::type_info* expectedType = nullptr;
+    const std::type_info *expectedType = nullptr;
+
 public:
     Chain() = default;
 
-    int sell() override {
-        if (this->cards.empty()) return 0;
+    int sell() override
+    {
+        if (this->cards.empty())
+            return 0;
         int numCards = this->cards.size();
         return this->cards.front()->getCardsPerCoin(numCards);
     }
 
-    bool isEmpty() const override {
+    bool isEmpty() const override
+    {
         return cards.empty();
     }
 
-    void print(std::ostream& out) const override {
-        if (!isEmpty()) {
-            out << cards[0]->getName() << ": ";  // Print the name of the chain
-            for (const Card* card : cards) {
-                card->print(out);  // Call print() of the card to print its specific representation
+    void print(std::ostream &out) const override
+    {
+        if (!isEmpty())
+        {
+            out << cards[0]->getName() << ": "; // Print the name of the chain
+            for (const Card *card : cards)
+            {
+                card->print(out); // Call print() of the card to print its specific representation
                 out << " ";
             }
-            out << std::endl;  // Add a newline at the end of the printed chain
-        } else {
-            out << "Empty chain" << std::endl;  // Handle empty chains
+            out << std::endl; // Add a newline at the end of the printed chain
+        }
+        else
+        {
+            out << "Empty chain" << std::endl; // Handle empty chains
         }
     }
 
-    Chain<T>& operator+=(Card* card){
+    Chain<T> &operator+=(Card *card)
+    {
         std::cout << "adding card to chain" << std::endl;
-        if (cards.empty()) {
+        if (cards.empty())
+        {
             expectedType = &typeid(*card);
-        } else {
-            if (*expectedType != typeid(*card)) {
+        }
+        else
+        {
+            if (*expectedType != typeid(*card))
+            {
                 throw IllegalTypeException();
             }
         }
         cards.push_back(card);
         return *this;
     }
+
+    template <typename T>
+    std::string getName(T *t)
+    {
+        // TODO:
+        // std::string s = typeid(*t).name();
+        // return;
+    }
 };
 
 template <typename T>
-std::ostream &operator<<(std::ostream &out, const Chain<T> &chain) {
-    if(!chain.isEmpty()) {
+std::ostream &operator<<(std::ostream &out, const Chain<T> &chain)
+{
+    if (!chain.isEmpty())
+    {
         out << chain.getName() << ": ";
-        for (Card* card : chain.cards) {
+        for (Card *card : chain.cards)
+        {
             out << *card << " ";
         }
     }
     return out;
-}
-
-template <typename T>
-std::string getName(T *t)
-{
-    // TODO:
-    // std::string s = typeid(*t).name(); 
-    // return;
 }
 
 #endif
